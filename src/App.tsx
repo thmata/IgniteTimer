a74e914f426b46e1b6b5cf8444c05032203d4f94
thmata <taiagomata@gmail.com>
2022-08-03 20:28:22 -0300

    Projeto

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <div className="App"></div>
+
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+}
